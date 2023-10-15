082a959547a6bca4c775b0b4b17f40142350b235
bendiazg <116295566+bendiazg@users.noreply.github.com>
2023-10-15 11:27:46 -0300

    Delete roverport directory
    reset

--- roverport/src/components/pages/BecomeSitter.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const BecomeSitter = () => {
-  return (
-    <div>BecomeSitter</div>
-  )
-}
--- roverport/src/components/pages/SearchSitter.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const SearchSitter = () => {
-  return (
-    <div>Search Sitter</div>
-  )
-}
